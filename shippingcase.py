f4de2410915db6250620b89dce0c6682eea7a06e
Sachin Acharya <thesachinacharya77@gmail.com>
2025-05-12 22:48:03 +0545

    Add new files

---
+++ shippingcase.py
@@ -0,0 +1,14 @@
+#this is used  If main.py is outside the utils     (mostly used)
+# package → import like import utils.shipping.
+
+
+
+# import utils.shipping
+# utils.shipping.calc_shipping()
+
+
+
+# or 
+
+# If main.py is inside the utils
+# package → import just import shipping.
---
+++ utils/_init_.py
@@ -0,0 +1,3 @@
+
+import utils.shipping
+utils.shipping.calc_shipping()
